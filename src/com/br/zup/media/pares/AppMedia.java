9a7e21d647236194a3784ad585bb4aea1e429385
Bruno <Bruno.cardoso@zup.com.br>
2019-06-03 14:52:30 -0300

    Adicionar If e Else

--- src/com/br/zup/media/pares/AppMedia.java
+++ src/com/br/zup/media/pares/AppMedia.java
@@ -1,25 +1,45 @@
 package com.br.zup.media.pares;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AppMedia {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = entrada();
-		System.out.println("Digite um numero para o calculo");
-		scan.nextInt();
-		
+
+		int total = 0;
+		int controle = 1;
+
+		List<Integer> numerosImpares = new ArrayList<Integer>();
+
+		while (controle == 1) {
+			System.out.println("Digite um numero para o calculo");
+			int numero = scan.nextInt();
+
+			if (numero == 0) {
+
+				controle = 0;
+
+			} else if (numero % 2 == 0) {
+				total += numero;
+			} else {
+				numerosImpares.add(numero);
+			}
+
+		}
 
 	}
-	
+
 	public static Scanner entrada() {
 		Scanner scan = new Scanner(System.in);
 		return scan;
 	}
-	
+
 	public static void calculaMedia(int divisor, int total) {
-		System.out.println("Media entre os numeros pares -> "+total/ divisor);
+		System.out.println("Media entre os numeros pares -> " + total / divisor);
 	}
 
 }
